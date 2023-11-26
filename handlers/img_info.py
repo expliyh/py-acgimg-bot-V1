a3f385fd68297f81aed4aeac89de252306b55619
Expliyh <expliyh@outlook.com>
2023-11-26 23:49:02 +0800

    Update img_info.py

--- handlers/img_info.py
+++ handlers/img_info.py
@@ -9,7 +9,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
 from config import config
-from pixiv import PixivAPI
+from pixiv import pixiv_api
 
 import database as db_class
 from database import *
@@ -22,8 +22,7 @@ async def get_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     cmd = context.args
     if cmd is None or cmd == "":
         return
-    api = PixivAPI()
-    tmp = await api.get_illust_info_by_pixiv_id(int(cmd[0]))
+    tmp = await pixiv_api.get_illust_info_by_pixiv_id(int(cmd[0]))
     message = str(tmp.__dict__)
     await context.bot.send_message(chat_id=config.developer_chat_id, text=message)
     return
